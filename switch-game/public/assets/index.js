05624d784b1eeddd3d8efa6a94138adbdcf37ea6
yuemintang <shirleytang1996@gmail.com>
2019-03-26 22:15:18 -0400

    commit 3
    set the window make it resizeable

--- switch-game/public/assets/index.js
+++ switch-game/public/assets/index.js
@@ -5,7 +5,7 @@ import Level1 from './scenes/Level1.js';
 var config = {
 	type: Phaser.AUTO,
 	width: 640,
-	height: 360,
+	height: 640,
 	scale: {
 		scale: 'SHOW_ALL',
 		orientation: 'LANDSCAPE'
@@ -21,7 +21,24 @@ var config = {
 			}
 		}
 	},
-	scene: [Intro, Level1]
+	scene: [Level1]
 };
 
+
 var game = new Phaser.Game(config);
+
+function resize() {
+	let canvas = document.querySelector('canvas');
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	let wratio = width / height;
+	let ratio = game.config.width / game.config.height;
+	if (wratio < ratio) {
+		canvas.style.width = width + 'px';
+		canvas.style.height = width / ratio + 'px';
+	} else {
+		canvas.style.width = height * ratio + 'px';
+		canvas.style.height = height + 'px';
+	}
+}
+
