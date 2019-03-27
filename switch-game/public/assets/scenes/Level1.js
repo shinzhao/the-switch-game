4b44891640ec75ef168ac735820b569d71908923
yuemintang <shirleytang1996@gmail.com>
2019-03-27 17:25:02 -0400

    commit13
    perload the chess

--- switch-game/public/assets/scenes/Level1.js
+++ switch-game/public/assets/scenes/Level1.js
@@ -6,6 +6,10 @@ export default class Level1 extends Phaser.Scene {
 	}
 	preload() {
 		this.load.image('boardbg', '/img/boardbg.png');
+		this.load.image('chess_red', '/img/chess_1.png');
+		this.load.image('chess_purple', '/img/chess_2.png');
+		this.load.image('chess_blue', '/img/chess_3.png');
+		this.load.image('chess_orange', '/img/chess_4.png');
 		this.load.spritesheet('cards', '/img/cards.png',
 		{frameWidth:334,
 		frameHeight:440,
