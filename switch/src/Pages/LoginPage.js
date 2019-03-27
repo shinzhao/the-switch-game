5fc65533ecda8f00c87cdd30135e03a9a27a6458
Yinuo Huang <noviahuang98@gmail.com>
2019-03-26 20:34:20 -0400

    login page (2) & bootstrap

--- switch/src/Pages/LoginPage.js
+++ switch/src/Pages/LoginPage.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, FormLabel} from 'react-bootstrap';
+import GuestPage from './GuestPage';
+import SignUpPage from './SignUpPage';
 
 class LoginPage extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            username: " ",
-            password: " "
+            username: "",
+            password: ""
         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    validateForm(){
+        return this.state.username.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange(event){
+        this.setState({[event.target.id]: event.target.value});
+    }
+
+    handleLogin(event){
+        //submit to database
+        event.preventDefault();
+    }
+
+    renderGuestPage(event){
+        window.location.href = "./GuestPage";
+    }
+
+    renderSignUpPage(event){
+        window.location.href = "./SignUpPage";
     }
 
     render(){
         return (
-            <div>
+            <div className="login-page">
                 <h1>WELCOME TO SWITCH</h1>
+                <form onSubmit={this.handleLogin}>
+                    <FormGroup controlId="username" bsSize="large">
+                        <FormLabel>Username: </FormLabel>
+                        <FormControl autoFocus type="text" value={this.state.username} onChange={this.handleChange} />
+                    </FormGroup>
+                    <br />
+                    <FormGroup controlId="password" bsSize="large">
+                        <FormLabel>Password: </FormLabel>
+                        <FormControl autoFocus type="password" value={this.state.password} onChange={this.handleChange} />
+                    </FormGroup>
+                    <br />
+                    <Button bsSize="large" disabled={!this.validateForm()} type="submit">Login</Button>
+                </form>
+                <Button className="guest-button" onClick={this.renderGuestPage}>Guest</Button>
+                <Button className="signup-button" onClick={this.renderSignUpPage}>Sign Up</Button>
             </div>
         );
     }
---
+++ switch/src/Pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends React.Component{
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default SignUpPage;
