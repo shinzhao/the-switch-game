4b53a82bba4706958304c0ca3b8adda47f67d574
jariclnnf <Rok.wxii@gmail.com>
2019-03-31 19:27:37 -0400

    config

---
+++ switch/src/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_rTMmLIX4K', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+		clientId: '23kopetofcotk9rg1hv22p9vi0' //is this used anywhere?
+    },
+};
+
