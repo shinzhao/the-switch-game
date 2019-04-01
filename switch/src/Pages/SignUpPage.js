9e35377251b3a6beab6b4b7d84294e26e9255476
jariclnnf <Rok.wxii@gmail.com>
2019-03-31 22:56:37 -0400

    debug

--- switch/src/Pages/SignUpPage.js
+++ switch/src/Pages/SignUpPage.js
@@ -27,7 +27,7 @@ class SignUpPage extends React.Component{
 
     }
 
-    handleChange(){
+    handleChange(event){
         this.setState({[event.target.id]: event.target.value});
     }
     render(){
