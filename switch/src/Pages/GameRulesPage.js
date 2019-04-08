9cae276cd98432c179ab41bd6142f73cc795725f
NoviaHuang <noviahuang98@gmail.com>
2019-04-08 11:17:01 -0400

    edit game rules page

--- switch/src/Pages/GameRulesPage.js
+++ switch/src/Pages/GameRulesPage.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
 class GameRulesPage extends React.Component {
-    
+    render() {
+        return (
+            <div className="game-rules">
+                <title>Game Rules</title>
+            </div>
+        );
+    }
 }
