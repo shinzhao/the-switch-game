a6e55e0ecccd490dac89fe7e58e4313d37264aff
NoviaHuang <noviahuang98@gmail.com>
2019-04-11 22:53:54 -0400

    synchronize with amplify branch, not my commit

--- switch/src/App.js
+++ switch/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import bg from './img/background.png'
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsmobile);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <img src={bg} className="App-bg" alt="background"/>
-          <p className="App-header">WELCOME TO SWITCH</p>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App,true);
