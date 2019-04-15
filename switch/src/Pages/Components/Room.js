3a87c5dc8a3a973f40eef07af1de069628c3b2eb
NoviaHuang <noviahuang98@gmail.com>
2019-04-14 20:02:48 -0400

    edit room list page

---
+++ switch/src/Pages/Components/Room.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Room extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            roomID: null,
+            player_count: null,
+            status: null
+        };
+
+    }
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default Room;
--- switch/src/Pages/RoomListPage.js
+++ switch/src/Pages/RoomListPage.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import main from '../App'
+import bg from '../img/background.png';
+import './RoomListPage.css';
+import Room from './Components/Room';
 
 class RoomListPage extends React.Component {
-    
+    constructor(){
+        super();
+        this.state={
+            roomID: null,
+            player_count: null,
+            status: null
+        };
+
+    }
+
+    getRoomID(){
+
+    }
+
+    getPlayerCount(){
+
+    }
+
+    getStatus(){
+
+    }
+
+    renderRoom(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+
     render() {
         return (
-            <Router>
-                <Button variant="outline-secondary" className="game-rules-button"><Link to="/game-rules">Game Rules</Link></Button>
-                <Button variant="primary" className="profile-button"><Link to="/profile">My Profile</Link></Button>
-                <Button variant="secondary" className="enter-room-num-button"><Link to="/room#">Enter Room Number</Link></Button>
-                <Button variant="secondary" className="new-room-button"><Link to="/new-room">Create a New Room</Link></Button>
-                <Button variant="secondary" className="random-button"><Link to="/random">Random Match</Link></Button>
-                
-                <Route path="/game-rules" component={GameRulesPage} />
-                <Route path="/profile" component={ProfilePage} />
-                <Route path="/room#" component={EnterRoomNumPage} />
-                <Route path="/new-room" component={NewRoomPage} />
-                <Route path="/random" component={RandomMatchPage} />
-            </Router>
+            <div className="App-bg">
+                <p className="App-header">WELCOME TO SWITCH</p>
+                <img src={bg} className="App-bg" alt="background"/>
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <Room />
+                <form>
+                    <label>Room #: <input type="number" name="room-num" /></label>
+                    <input type="submit" value="ENTER" />
+                </form>
+                <button>Create New Room</button>
+                <button>Random Match</button>
+            </div>
         );
     }
 }
