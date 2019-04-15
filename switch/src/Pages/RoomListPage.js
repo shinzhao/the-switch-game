ab65452bb9585421598f326dc583080fb1a86bc7
NoviaHuang <noviahuang98@gmail.com>
2019-04-14 23:12:11 -0400

    almost finished with room list page, need to retrieve room data from database

--- switch/src/Pages/RoomListPage.js
+++ switch/src/Pages/RoomListPage.js
@@ -1,53 +1,42 @@
 import React, { Component } from 'react';
 import bg from '../img/background.png';
 import './RoomListPage.css';
-import Room from './Components/Room';
 
 class RoomListPage extends React.Component {
     constructor(){
         super();
         this.state={
-            roomID: null,
-            player_count: null,
-            status: null
+            roomID: getRoomID(),
+            player_count: getPlayerCount(),
+            status: getStatus()
         };
         this.handleClick = this.handleClick.bind(this);
     }
-
-    getRoomID(){
-
-    }
-
-    getPlayerCount(){
-
-    }
-
-    getStatus(){
-
-    }
-
+    
+    
     handleClick(){
-        this.setState({
-            roomID: 1,
-            player_count: 4,
-            status: "playing"
-        });
+        alert("button clicked");
     }
 
     renderRoom(){
         return(
             <div>
-                
+                <button className="room" onClick={this.handleClick}>
+                    Room: {this.state.roomID}
+                    {this.state.player_count}/4
+                    {this.state.status}
+                </button>
             </div>
         );
     }
 
     render() {
+        
         return (
             <div className="room-list">
                 <p className="room-header">SWITCH</p>
                 <img src={bg} className="room-bg" alt="background"/>
-                
+                {this.renderRoom(0)}
                 <button>Prev</button>
                 <button>Next</button>
                 <form>
@@ -59,6 +48,19 @@ class RoomListPage extends React.Component {
             </div>
         );
     }
+}
+
+//retrieve all data from database
+function getRoomID(){
+    return [1];
+}
+
+function getPlayerCount(){
+    return [4];
+}
+
+function getStatus(){
+    return ['playing'];
 }
 
 export default RoomListPage;
