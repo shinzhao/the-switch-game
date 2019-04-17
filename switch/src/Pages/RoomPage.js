aa5169b5f05b4a8e95364f14c5b52055a852d7af
yuemintang <shirleytang1996@gmail.com>
2019-04-17 11:20:03 -0400

    commit 35
    combine gameboard file to amplify file and login page

---
+++ switch/src/Pages/RoomPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Game from './phaser/Game';
+
+class RoomPage extends Component {
+    render() {
+        return(
+            <div>
+                <p>hello</p>
+            </div>
+        );
+    }
+}
+
+export default RoomPage;
