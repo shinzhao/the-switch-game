d0335f0b09ba87b14576f303d38025071a356357
david_huang <jiahaodavid4040@gmail.com>
2019-04-18 05:02:49 -0400

    demo appsnyc based on the gameboard

---
+++ react-version/src/phaser/scenes/Load.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+
+export class Load extends Phaser.Scene{
+    constructor(){
+        super({
+            key: 'Load'
+        });
+    }
+    init(){
+        
+    }
+
+    preload(){
+        this.load.image('boardbg', `${process.env.PUBLIC_URL}/assets/boardbg.png`);
+		this.load.image('blank',`${process.env.PUBLIC_URL}/assets/blabk.jpg`)
+		this.load.image('chess_red', `${process.env.PUBLIC_URL}/assets/chess_1.png`);
+		// this.load.image('chess_purple', './img/chess_2.png');
+		// this.load.image('chess_blue', './img/chess_3.png');
+		// this.load.image('chess_orange', './img/chess_4.png');
+		this.load.spritesheet('cards', `${process.env.PUBLIC_URL}/assets/cards.png`,
+		{frameWidth:334,
+		frameHeight:440,
+		});
+
+    }
+
+    create(){
+        this.scene.start('GameBoard')
+    }
+    update(){
+
+    }
+}
