24b24d0fa617b18cf0c8a440baf31efc59059660
jariclnnf <Rok.wxii@gmail.com>
2019-04-20 23:43:08 -0400

    update

--- switch/src/Pages/ProfilePage.js
+++ switch/src/Pages/ProfilePage.js
@@ -19,6 +19,34 @@ class profile extends Component {
         </div>
       <p>Win: | Lost: | Presentage: |</p>
       {/* game records */}
+      <div className="social-links">
+    
+      <div>
+          <p>name</p>
+          <input placeholder="username"></input>
+          {/* username should placehold to the current name */}
+      </div>
+      <div>
+          <p>Email</p>
+          <input placeholder="email"></input>
+          {/*email should placehold to the current name */}
+      </div>
+      <div>
+          <p>Gender</p>
+          <input required type="radio" value="male"></input>Male
+          <input required type="radio" value="female"/>Female
+      </div>
+      <div>
+          <p>Password</p>
+          <input placeholder="8-10 chatacters"></input>
+      </div>
+      <div>
+          <p>Confirm Password</p>
+          <input></input>
+      </div>
+      <button type="submit">Save</button>
+    </div>
+        
       </Cell>
     </Grid>
 
