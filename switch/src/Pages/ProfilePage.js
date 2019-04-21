d68a6222bff12ff9d97eca88e351020625bdae00
jariclnnf <Rok.wxii@gmail.com>
2019-04-20 23:29:44 -0400

    grid cell

---
+++ switch/src/Pages/ProfilePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class profile extends Component {
+  render() {
+    return (
+        <div className="App">
+        <div style={{width: '100%', margin: 'auto'}}>
+    <Grid className="landing-grid">
+      <Cell col={12}>
+       
+      </Cell>
+    </Grid>
+
+        </div>
+        </div>
+    )
+}
+}
+
+export default profile;
