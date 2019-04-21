fcb4c53ee82d9347bb20a98a7e0d9c6e312f7afd
jariclnnf <Rok.wxii@gmail.com>
2019-04-20 23:32:37 -0400

    update

--- switch/src/Pages/ProfilePage.js
+++ switch/src/Pages/ProfilePage.js
@@ -7,7 +7,18 @@ class profile extends Component {
         <div style={{width: '100%', margin: 'auto'}}>
     <Grid className="landing-grid">
       <Cell col={12}>
-       
+      <img
+          src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+          alt="profile-img"
+          className="profile-img"
+          />
+
+        <div>
+          <h1>username</h1>
+        <hr/>
+        </div>
+      <p>Win: | Lost: | Presentage: |</p>
+      {/* game records */}
       </Cell>
     </Grid>
 
