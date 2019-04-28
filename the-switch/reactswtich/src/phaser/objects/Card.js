9f74d8f88f4fbf1cc97a5e81786defb62b0d4b75
yuemintang <shirleytang1996@gmail.com>
2019-04-28 13:24:20 -0400

    add left card function

--- the-switch/reactswtich/src/phaser/objects/Card.js
+++ the-switch/reactswtich/src/phaser/objects/Card.js
@@ -1,8 +1,12 @@
 import Phaser from 'phaser';
 export class Card extends Phaser.GameObjects.Sprite{
-    constructor(scene,x,y,key,target){
+    constructor(scene,x,y,key,target,left=36){
         super(scene,x,y,key,target);
         scene.sys.displayList.add(this);
         this.setScale(0.13,0.13).setInteractive().setDataEnabled()
+        this.left=left
+    }
+    moveCard(){
+        this.lefe-=1
     }
 }
