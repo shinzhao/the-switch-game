7e091bd88e6ab420491ef377aaf1ae3d521d6e97
jariclnnf <Rok.wxii@gmail.com>
2019-04-29 21:08:04 -0400

    merge

--- switch/src/Pages/ProfilePage.js
+++ switch/src/Pages/ProfilePage.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
-import RoomListPage from './RoomListPage';
-import './RoomPage.css';
+import { Grid, Cell } from 'react-mdl';
 
-class ProfilePage extends React.Component {
-    constructor() {
-        super();
-        this.state={
-            enableRoomListPage: false,
-            enableProfilePage: true
-        }
-        this.handleBackClick = this.handleBackClick.bind(this);
-        this.showProfilePage = this.showProfilePage.bind(this);
-    }
+class profile extends Component {
+  render() {
+    return (
+        <div className="App">
+        <div style={{width: '100%', margin: 'auto'}}>
+    <Grid className="landing-grid">
+      <Cell col={12}>
+      <img
+          src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+          alt="profile-img"
+          className="profile-img"
+          />
 
-    handleBackClick() {
-        this.setState({ 
-            enableRoomListPage: true,
-            enableProfilePage: false
-         })
-    }
+        <div>
+          <h1>username</h1>
+        <hr/>
+        </div>
+      <p>Win: | Lost: | Presentage: |</p>
+      {/* game records */}
+      <div className="info">
+    
+      <div>
+          <p>name</p>
+          <input placeholder="username"></input>
+          {/* username should placehold to the current name */}
+      </div>
+      <div>
+          <p>Email</p>
+          <input placeholder="email"></input>
+          {/*email should placehold to the current name */}
+      </div>
+      <div>
+          <p>Gender</p>
+          <input required type="radio" value="male"></input>Male
+          <input required type="radio" value="female"/>Female
+      </div>
+      <div>
+          <p>Password</p>
+          <input placeholder="8-10 chatacters"></input>
+      </div>
+      <div>
+          <p>Confirm Password</p>
+          <input></input>
+      </div>
+      <button type="submit">Save</button>
+    </div>
+        
+      </Cell>
+    </Grid>
 
-    showProfilePage() {
-        return(
-            <div>
-                <p className="test">This is the profile page</p>
-                <button onClick={this.handleBackClick}>Back</button>
-            </div>
-        )
-    }
-
-
-    render() {
-        return(
-            <div>
-                { this.state.enableProfilePage ? this.showProfilePage() : null}
-                { this.state.enableRoomListPage ? <RoomListPage /> : null }
-            </div>
-        );
-    }
+        </div>
+        </div>
+    )
+}
 }
 
-export default ProfilePage;
+export default profile;
