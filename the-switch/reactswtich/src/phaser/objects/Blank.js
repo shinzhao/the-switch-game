2481ffffc28f3dee86d88c0dd8a54197e154e3f8
yuemintang <shirleytang1996@gmail.com>
2019-04-30 14:58:36 -0400

    nothing

--- the-switch/reactswtich/src/phaser/objects/Blank.js
+++
@@ -1,9 +0,0 @@
-import Phaser from 'phaser';
-export class Blank extends Phaser.GameObjects.Image{
-    constructor(scene,x,y,key){
-        super(scene,x,y,key);
-        scene.sys.displayList.add(this);
-        this.setScale(0.1,0.15).setInteractive().setDataEnabled().setVisible(true);
-    }
-    
-}
