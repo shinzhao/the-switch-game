3b44df150428bb92899adaa32658e587a9ce519e
jariclnnf <Rok.wxii@gmail.com>
2019-05-01 21:51:31 -0400

    test

--- switch/src/App.test.js
+++ switch/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from 'react-testing-library'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
