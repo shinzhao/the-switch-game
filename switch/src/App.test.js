ceb1f6e4549d493b2438e2dbdbb0ba08b418f61c
jariclnnf <Rok.wxii@gmail.com>
2019-05-01 22:42:48 -0400

    debug

--- switch/src/App.test.js
+++ switch/src/App.test.js
@@ -1,9 +1,9 @@
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
 import React from 'react';
 import App from './App';
 import {render} from 'react-testing-library'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />)
 });
