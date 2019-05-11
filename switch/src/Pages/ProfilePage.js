16bbead9f39be99b10c9462a7c75217bde8729d7
jariclnnf <Rok.wxii@gmail.com>
2019-05-10 21:34:19 -0400

    profile update

--- switch/src/Pages/ProfilePage.js
+++ switch/src/Pages/ProfilePage.js
@@ -6,6 +6,16 @@ import './ProfilePage.css';
 import { Auth } from 'aws-amplify';
 
 class ProfilePage extends Component {
+  state={
+    oldpassword: "",
+    newpassword: "",
+    confirmpassword: "",
+    errors: {
+      cognito: null,
+      blankfield: false,
+      passwordmatch: false
+    }
+  }
   constructor(){
     super();
     this.state=({
@@ -49,8 +59,26 @@ class ProfilePage extends Component {
 
                 <div className="username">
                     <h1>{this.state.name}</h1>
+
                 </div>
                 <p className="score">Win: | Lost: | Presentage: |</p>
+                
+                <div className="passwordsetting">
+                <h1>Change Password</h1>
+                <div className="oldpassword">
+                <input className="input" type="password" id="oldpassword" placeholder="Old Password"></input>
+                </div>
+                <div className="newpassword">
+                <input className="input" type="password" id="newpassword" placeholder="New Password"></input>
+                </div>
+                <div className="confirmpassword">
+                <input className="input" type="password" id="confirmpassword" placeholder="Confirm Paaword"></input>
+                </div>
+                <div className="submitbutton">
+                <button className="submit" type="submit" id="submit">Submit</button>
+                </div>
+                </div>
+
         </div>
     )
 }
