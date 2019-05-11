194a59aadf63cde90a8bae18f9a0d9a3751eed0a
jiahaodavid <45926410+jiahaodavid@users.noreply.github.com>
2019-05-11 19:40:41 -0400

    delete

--- switch/node_modules/aws-appsync/lib/link/complex-object-link.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'apollo-link';
-import { ApolloLink } from 'apollo-link';
-export declare class ComplexObjectLink extends ApolloLink {
-    private link;
-    constructor(credentials: any);
-    request(operation: any, forward: any): Observable<import("apollo-link/lib/types").FetchResult<Record<string, any>, Record<string, any>>>;
-}
-export declare const complexObjectLink: (credentials: any) => ApolloLink;
