12cfe44575e54f93a37b354e313a864ef38a64cd
jariclnnf <Rok.wxii@gmail.com>
2019-05-11 00:07:03 -0400

    update

--- switch/src/Pages/PasswordConfirmationPage.js
+++ switch/src/Pages/PasswordConfirmationPage.js
@@ -2,27 +2,30 @@ import React, { Component } from "react";
 import './PasswordConfirmationPage.css'
 
 
+
 class PasswordConfirmationPage extends Component {
   handleBackClick(e){
     e.preventDefault();
-    this.props.history.push('/my-account')
+    this.props.history.push('my-account')
   }
 
   handleGameRuleClick(e){
     e.preventDefault();
-    this.props.history.push('/game-rule')
+    this.props.history.push('game-rule')
   }
 
   handleProfileClick(e){
     e.preventDefault();
-    this.props.history.push('/my-account')
+    this.props.history.push('my-account')
   }
   render() {
     return ( 
       <div className="confirmation-page">
-        <button className="back-button" onClick={this.handleBackClick}>Back</button>
-        <button className="game-rule-button" onClick={this.handleGameRuleClick}>Game Rule</button>
-        <button className="profile-button" onClick={this.handleProfileClick}>My Account</button>    
+      <div className="button-section">
+        <button className="Back-button" onClick={this.handleBackClick}>Back</button>
+        <button className="Game-rule-button" onClick={this.handleGameRuleClick}>Game Rule</button>
+        <button className="Profile-button" onClick={this.handleProfileClick}>My Account</button>
+        </div>    
         <div className="MessageArea">
           <h1>Change Password</h1>
           <p>Your password has been successfully updated!</p>
