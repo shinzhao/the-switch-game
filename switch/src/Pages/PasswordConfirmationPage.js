1629e3237e169ef39e57cf1a3029827089b4fcb5
jariclnnf <Rok.wxii@gmail.com>
2019-05-10 23:46:12 -0400

    update confirmation page

--- switch/src/Pages/PasswordConfirmationPage.js
+++ switch/src/Pages/PasswordConfirmationPage.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import './PasswordConfirmationPage.css'
 
 
 class PasswordConfirmationPage extends Component {
+  handleBackClick(e){
+    e.preventDefault();
+    this.props.history.push('/my-account')
+  }
+
+  handleGameRuleClick(e){
+    e.preventDefault();
+    this.props.history.push('/game-rule')
+  }
+
+  handleProfileClick(e){
+    e.preventDefault();
+    this.props.history.push('/my-account')
+  }
   render() {
-    return (
-      <section className="section auth">
-        <div className="container">
+    return ( 
+      <div className="confirmation-page">
+        <button className="back-button" onClick={this.handleBackClick}>Back</button>
+        <button className="game-rule-button" onClick={this.handleGameRuleClick}>Game Rule</button>
+        <button className="profile-button" onClick={this.handleProfileClick}>My Account</button>    
+        <div className="MessageArea">
           <h1>Change Password</h1>
           <p>Your password has been successfully updated!</p>
         </div>
-      </section>
+      </div>
     );
   }
 }
