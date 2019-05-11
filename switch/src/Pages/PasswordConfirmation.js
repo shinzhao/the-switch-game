f322b16db9e37a1691ccf6bb1d9ad2a667a1b627
jariclnnf <Rok.wxii@gmail.com>
2019-05-10 21:40:52 -0400

    change password confirm page

---
+++ switch/src/Pages/PasswordConfirmation.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class PasswordConfirmation extends Component {
+  render() {
+    return (
+      <section className="section auth">
+        <div className="container">
+          <h1>Change Password</h1>
+          <p>Your password has been successfully updated!</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default PasswordConfirmation;
