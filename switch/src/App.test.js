b0f8b8c2cffc0c016b58c06df6fe99e10fb0e0a4
JinFeng Lin <44300311+jariclnnf@users.noreply.github.com>
2019-05-12 16:36:30 -0400

    Update App.test.js

--- switch/src/App.test.js
+++ switch/src/App.test.js
@@ -5,6 +5,7 @@ import App from './App';
 import {render} from 'react-testing-library'
 const puppeteer = require('puppeteer');
 
+jest.setTimeout(30000);
 test("login successfully", async()=>{
   const browser = await puppeteer.launch({
         headless: false,
@@ -12,8 +13,14 @@ test("login successfully", async()=>{
         args: ['--window-size=1920,1080']
       });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
-  await page.click("button.login-button");
+  await page.goto('http://localhost:3000/room-list');
+  await page.waitFor('input[name=username]');
+  await page.type('input[name=username]','jaric');
+  await page.waitFor('input[name=password]');
+  await page.type('input[name=password]', 'Rok_W0900');
+  await page.click('button[type=submit]');
+  await page.waitFor(300);
   await page.url('http://localhost:3000/room-list');
   browser.close();
 });
+
