31610c110db77b7695972dd6f06b4ac798f04f86
jariclnnf <Rok.wxii@gmail.com>
2019-05-11 23:44:08 -0400

    E2E test

---
+++ switch/src/Tests/RoomListPage.test.js
@@ -0,0 +1,6 @@
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+import React from 'react';
+import ProfilePage from '../Pages/RoomListPage';
+import {render} from 'react-testing-library'
+const puppeteer = require('puppeteer');
